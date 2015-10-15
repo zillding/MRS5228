d44723a1b0a1e4ebee69d0e7583cbc185c4ce477
Zill Ding <zilldzy@gmail.com>
2015-10-15 16:32:32 +0800

    Highlight the input & output file path

--- parse_to_csv.py
+++ parse_to_csv.py
@@ -1,5 +1,9 @@
 import csv
 
+input_file_path = './movies.txt'
+output_file_path = './movie_reviews.csv'
+
+input_file = open(input_file_path)
 cols = [
     'product_productid',
     'review_userid',
@@ -11,8 +15,8 @@ cols = [
     # 'review_text'
     ]
 
-f = open("movie_reviews.csv", "wb")
-w = csv.writer(f)
+output_file = open(output_file_path, 'wb')
+w = csv.writer(output_file)
 w.writerow(cols) # write table header first
 
 def write_row(doc):
@@ -20,22 +24,28 @@ def write_row(doc):
 
 count = 0
 doc = {}
-for line in open("./movies.txt"):
+for line in input_file:
     line = line.strip()
-    if line == "":
+    if line == '':
         write_row(doc)
         doc = {}
         count += 1
         if count % 100000 == 0:
-            print "Number of records processed:", count
+            print 'Number of records processed:', count
     else:
-        idx = line.find(":")
+        idx = line.find(':')
         key, value = tuple([line[:idx], line[idx+1:]])
-        key = key.strip().replace("/", "_").lower()
+        key = key.strip().replace('/', '_').lower()
         value = value.strip()
         doc[key] = value
+
+# write the last row if there is one
 if doc:
-    write_row(doc) # write the last row if there is one
+    write_row(doc)
     count += 1
-print "Total number of records:", count
-f.close()
+
+print 'Total number of records:', count
+
+# close files
+input_file.close()
+output_file.close()
