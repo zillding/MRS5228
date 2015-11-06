6c0bdbb997995dcb748a59b5a787c704993ed1a1
Zill Ding <zilldzy@gmail.com>
2015-11-06 12:05:10 +0800

    Add check whether user id exist in data

--- find_similar_users.py
+++ find_similar_users.py
@@ -16,14 +16,21 @@ df = pd.read_csv(input_file_path)
 table = pd.pivot_table(df, values='review_score', index=['product_productid'], columns=['review_userid']).fillna(0)
 logger.info('Done loading!')
 
-target_user_rating = table[target_userid]
-# compute similarity between two vectors
-def calc_sim(rating):
-    # compute cosine similarity between target_user_rating and rating
-    result = 1 - spatial.distance.cosine(target_user_rating, rating)
-    return result
+if not target_userid in table:
+    logger.critical('Cannot find such user in data')
+    logger.info('Exit program')
+    exit()
+else:
+    logger.info('Start finding similar users...')
 
-result = table.apply(calc_sim).drop(target_userid).sort_values(ascending=False)[:k]
+    target_user_rating = table[target_userid]
+    # compute similarity between two vectors
+    def calc_sim(rating):
+        # compute cosine similarity between target_user_rating and rating
+        result = 1 - spatial.distance.cosine(target_user_rating, rating)
+        return result
 
-logger.info('Done processing')
-logger.info('Result: ' + result.to_json())
+    result = table.apply(calc_sim).drop(target_userid).sort_values(ascending=False)[:k]
+
+    logger.info('Done processing')
+    logger.info('Result: ' + result.to_json())
